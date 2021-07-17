498e10dc9e45a212364e698e063c38da9cf251ba
fshatskiy <f.shatskiy@students.ephec.be>
2021-07-17 22:57:53 +0200

    init

--- ECGS/asgi.py
+++ ECGS/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ecgs project.
+ASGI config for ECGS project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecgs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ECGS.settings')
 
 application = get_asgi_application()
--- ECGS/wsgi.py
+++ ECGS/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ecgs project.
+WSGI config for ECGS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecgs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ECGS.settings')
 
 application = get_wsgi_application()
